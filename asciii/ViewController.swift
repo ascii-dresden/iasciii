f3d7369c64bc3bcdc58dc26a72cad7a4254fb164
Hendrik Sollich <hendrik.sollich@logmein.com>
2018-03-30 11:48:47 +0200

    Initial Commit

---
+++ asciii/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  asciii
+//
+//  Created by Hendrik Sollich on 30.03.18.
+//  Copyright © 2018 Hendrik Sollich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
